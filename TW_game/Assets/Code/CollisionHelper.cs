50d6a9c67cb6bf0718fa48d93105d1354f197d2b
ArgyleSocks <maximtar@gmail.com>
2021-10-13 13:35:13 -0700

    Added collider intersection detection, can begin adding enemy-player interaction

--- TW_game/Assets/Code/CollisionHelper.cs
+++ TW_game/Assets/Code/CollisionHelper.cs
@@ -9,13 +9,13 @@ public class CollisionHelper : MonoBehaviour
   public bool collision = false;
   Collision2D foreign;
 
-  private void OnCollisionEnter2D(Collision2D foreign_) {
+  void OnCollisionEnter2D(Collision2D foreign_) {
     Debug.Log("COLLIDING");
     collision = true;
     foreign = foreign_;
   }
 
-  private void OnCollisionExit2D(Collision2D foreign_) {
+  void OnCollisionExit2D(Collision2D foreign_) {
     collision = false;
   }
 }
